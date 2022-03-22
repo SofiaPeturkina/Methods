6d53c876c28465e4b43eef9fd8a901067db238f2
Sofia Peturkina <peturkina@bk.ru>
2022-03-22 19:17:30 +0300

    Solution task Methods

--- src/js/Bowman.js
+++ src/js/Bowman.js
@@ -2,7 +2,7 @@ import Character from './Character';
 
 export default class Bowman extends Character {
     constructor(name) {
-        super(name, "Bowman");
+        super(name,"Bowman");
         this.attack = 25;
         this.defence = 25;
     }
